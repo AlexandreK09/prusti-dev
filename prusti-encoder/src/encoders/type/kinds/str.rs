160070ebc1f19d193bf779c7fcb30b22b28e64aa
AlexandreK09 <alexkejikian@gmail.com>
2025-06-02 14:01:13 +0200

    added get_all_UnsafeCells for str

--- prusti-encoder/src/encoders/type/kinds/str.rs
+++ prusti-encoder/src/encoders/type/kinds/str.rs
@@ -74,6 +74,22 @@ pub(crate) fn predicate<'vir>(
             .1,
     );
 
+    builder.get_unsafe_cells = Some(
+        builder
+            .mk_function(
+                "get_all_UnsafeCells", 
+                &[ref_self_decl], 
+                builder.vcx.mk_ty_set(&vir::TypeData::Ref), 
+                &[vir::expr! { acc_wildcard([self_pred](ref_self)) }], 
+                &[], 
+                Some(
+                    vir::expr! {
+                        Set([&vir::TypeData::Ref]())
+                    }
+                )
+            )
+    );
+
     Ok(PredicateEncData::StructLike(PredicateEncDataStruct {
         snap_data,
         ref_to_field_refs: builder.vcx.alloc_slice(&field_accessors),
